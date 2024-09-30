6e75ad181083ba04a04a2f8ba5e462d6501fd1c5
Jose Arturo <arturotoll5@gmail.com>
2024-09-30 01:57:56 -0500

    asdf

--- src/routes/classes/[class_id]/courses/[class_season_level_course_id]/behaviors/+page.ts
+++
@@ -1,8 +0,0 @@
-import { error } from '@sveltejs/kit';
-
-export const load = async ({ parent, params: { class_season_level_course_id } }) => {
-  const { supabase } = await parent()
-  const { data: behaviors, error: err } = await supabase.from("behaviors").select("*").eq("class_season_course_id", class_season_level_course_id)
-  if (err) throw error(500, err)
-  return { behaviors }
-};
